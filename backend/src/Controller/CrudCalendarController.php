6ab50bad10251ed72383e2f11fe7eb5480d11888
Papoel <pascal.briffard@gmail.com>
2023-08-07 22:50:32 +0200

    👌 IMPROVE: réservés aux administrateurs

--- backend/src/Controller/CrudCalendarController.php
+++ backend/src/Controller/CrudCalendarController.php
@@ -9,8 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/calendrier')]
+#[isGranted('ROLE_ADMIN')]
 class CrudCalendarController extends AbstractController
 {
     #[Route('/', name: 'app_calendar_index', methods: ['GET'])]
