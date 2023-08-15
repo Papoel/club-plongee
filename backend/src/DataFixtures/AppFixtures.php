54eb5109bc4e60dc1adc2822ec7c43413d1dab24
Papoel <pascal.briffard@gmail.com>
2023-08-15 21:51:56 +0200

    💡 REFACTOR: Modification suite à MAJ Entité User

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -23,23 +23,29 @@ class AppFixtures extends Fixture
 
         $admin = new User();
         $admin
+            ->setGenre(genre: (string) 'Homme')
             ->setFirstname(firstname: 'pascal')
             ->setLastname(lastname: 'briffard')
             ->setEmail(email: 'papoel@admin.fr')
-            ->setRoles(roles: ['ROLE_ADMIN'])
+            ->setRoles(roles: ['ROLE_ADMIN', 'ROLE_ADHERENT'])
             ->setAddress(address: '15 rue de la Liberté')
-            ->setZipCode(zipCode: '59600')
-            ->setCity(city: 'Maubeuge');
+            ->setZipCode(zipCode: (string) '59600')
+            ->setCity(city: 'Maubeuge')
+            ->setCountry(country: (string) 'France')
+            ->setPhone(phone: '0669399414')
+            ->setBio(bio: 'Je suis le super admin')
+        ;
+
         /** @param array $certificats */
         $certificats = [
             'certificat_medical_2021.pdf',
             'certificat_medical_2022.pdf',
             'certificat_medical_2023.pdf',
         ];
-        /* @phpstan-ignore-next-line */
+        // /* @phpstan-ignore-next-line */
         $admin->setCertificateMedical(certificateMedical: $faker->randomElement($array = $certificats));
 
-        $hash = $this->passwordHasher->hashPassword(user: $admin, plainPassword: 'admin');
+        $hash = $this->passwordHasher->hashPassword(user: $admin, plainPassword: 'admin1234');
         $admin->setPassword(password: $hash);
 
         // Ajouter une licence à l'admin
@@ -52,25 +58,36 @@ class AppFixtures extends Fixture
         $manager->persist($admin);
         $users[] = $admin;
 
-        // Generate 10 users
-        for ($adherent = 0; $adherent < 10; ++$adherent) {
+        // Generate 5 users
+        for ($adherent = 1; $adherent <= 5; ++$adherent) {
             $user = new User();
+            $genre = $faker->randomElement(['Homme', 'Femme', 'Autre']);
+            $country = $faker->randomElement(['France', 'Belgique']);
+            $divingLevel = $faker->randomElement(['1', '2', '3']);
+
             $user
+                /* @phpstan-ignore-next-line */
+                ->setGenre(genre: (string) $genre)
                 ->setFirstname(firstname: $faker->firstName())
                 ->setLastname(lastname: $faker->lastName())
-                ->setEmail(email: $faker->email())
+                ->setEmail(email: 'user'.$adherent.'@test.fr')
                 ->setAddress(address: $faker->streetAddress())
-                // Transforme le code postal en string pour éviter les erreurs de type
                 ->setZipCode(zipCode: (string) $faker->randomNumber($nbDigits = 5, $strict = true))
-                ->setCity(city: $faker->city());
+                ->setCity(city: $faker->city())
+                /* @phpstan-ignore-next-line */
+                ->setCountry(country: (string) $country)
+                /* @phpstan-ignore-next-line */
+                ->setDivingLevel(diving_level: (int) $divingLevel)
+                ->setPhone(phone: $faker->phoneNumber())
+            ;
 
-            /** @param array $certificats */
+            // /** @param array $certificats */
             $certificats = [
                 'certificat_medical_2021.pdf',
                 'certificat_medical_2022.pdf',
                 'certificat_medical_2023.pdf',
             ];
-            /* @phpstan-ignore-next-line */
+            // /* @phpstan-ignore-next-line */
             $admin->setCertificateMedical(certificateMedical: $faker->randomElement($array = $certificats));
             $hash = $this->passwordHasher->hashPassword(user: $user, plainPassword: 'plongee');
             $user->setPassword(password: $hash);
