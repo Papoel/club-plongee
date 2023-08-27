2c0c00bd91b63756d50f181270bb772637846e96
Papoel <pascal.briffard@gmail.com>
2023-08-27 12:58:32 +0200

    👌 IMPROVE: Mise en place du cookie RememberMe

--- backend/src/Security/LoginAuthenticator.php
+++ backend/src/Security/LoginAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -35,6 +36,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
             new UserBadge(userIdentifier: (string) $email),
             credentials: new PasswordCredentials(password: (string) $request->request->get(key: 'password', default: '')),
             badges: [
+                new rememberMeBadge(),
                 new CsrfTokenBadge(csrfTokenId: 'authenticate', csrfToken: (string) $request->request->get(key: '_csrf_token')),
             ]
         );
